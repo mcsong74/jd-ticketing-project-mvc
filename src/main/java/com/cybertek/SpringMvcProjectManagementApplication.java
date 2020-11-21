79201ba9f8b302f38e8dcd0c836385e0130daf9d
ssonata74 <ssonata@gmail.com>
2020-11-20 23:44:23 -0500

    Commit test

--- src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
+++ src/main/java/com/cybertek/SpringMvcProjectManagementApplication.java
@@ -3,6 +3,7 @@ package com.cybertek;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ public class SpringMvcProjectManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcProjectManagementApplication.class, args);
+		System.out.println("Hello");
 	}
 
 
