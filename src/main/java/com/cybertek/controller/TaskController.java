80719e4e6e938a0b7682f1ef213ed56f0b42f873
ssonata74 <ssonata@gmail.com>
2020-11-28 01:31:14 -0500

    Ticketing Project - mvc - Task

--- src/main/java/com/cybertek/controller/TaskController.java
+++ src/main/java/com/cybertek/controller/TaskController.java
@@ -4,13 +4,18 @@ import com.cybertek.dto.TaskDTO;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.TaskService;
 import com.cybertek.service.UserService;
+import com.cybertek.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
 @Controller
@@ -36,12 +41,30 @@ public class TaskController {
 
     @PostMapping("/create")
     public String addTask(TaskDTO task, Model model) {
-        task.setTaskId("TASK-" + taskService.findAll().stream().count() + 1);
+        task.setTaskId("TASK-" + (taskService.findAll().stream().count() + 1l));
+        task.setAssignedDate(LocalDateTime.now());
         taskService.save(task);
+        task.setStatus(Status.OPEN);
 
-        return "redirect:task/create";
+        return "redirect:/task/create";
     }
 
+    @GetMapping("/delete/{taskId}")
+    public String deleteTask(TaskDTO task){
+        taskService.deleteById(task.getTaskId());
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") String taskId, Model model){
+        model.addAttribute("task", taskService.findById(taskId));
+        model.addAttribute("projectlist", projectService.findAll());
+        model.addAttribute("employeelist", userService.findAll().stream()
+                .filter(user -> user.getRole().getDescription().equals("Employee"))
+                .collect(Collectors.toList()));
+        model.addAttribute("tasklist", taskService.findAll());
+        return "/task/update";
+    }
 
     @GetMapping("/pending")
     public String pendingTasks() {
